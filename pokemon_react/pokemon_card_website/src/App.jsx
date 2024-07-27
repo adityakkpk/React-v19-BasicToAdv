dffe5738db8ba67904b5b62aa34e97242e399c75
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-07-27 22:11:59 +0530

    Complete Pokemon Project

--- pokemon_react/pokemon_card_website/src/App.jsx
+++ pokemon_react/pokemon_card_website/src/App.jsx
@@ -1,11 +1,78 @@
-import { useState } from 'react'
-import Pokemon from './Pokemon'
+import { useEffect, useState } from 'react'
+import PokemonCard from './Components/PokemonCard';
 
 function App() {
 
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+
+  const API = 'https://pokeapi.co/api/v2/pokemon?limit=124';
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json(); 
+      // console.log(data); 
+
+      const detailedPokemonData = data.results.map(async (currData) =>{
+        const res = await fetch(currData.url);
+        const data = await res.json();
+        // console.log(data);
+        setLoading(false);
+        return data;
+      });
+
+      const detailedResponse = await Promise.all(detailedPokemonData);
+      setPokemon(detailedResponse);
+
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  // Search Functionality
+  const searchData = pokemon.filter((currPokemon) => currPokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+  if(loading) {
+    return <div className='w-full h-[100vh] overflow-y-hidden flex items-center justify-center'>
+      <h1 className='text-4xl'>Loading...</h1>
+    </div>
+  }
+  
+  if(error) {
+    return <div className='w-full h-[100vh] overflow-y-hidden flex items-center justify-center'>
+      <h1 className='text-4xl'>{error.message}</h1>
+    </div>
+  }
+
   return (
-    <Pokemon />
-  )
+    <div className="w-full min-h-screen flex gap-10 flex-col justify-start py-10 px-2 items-center bg-gray-200">
+      <h1 className="text-4xl font-semibold">Lets Catch Pokemon</h1>
+      <input
+        type="text"
+        className="border-b-4 border-sky-500 focus:outline-none px-2 py-1 w-64 rounded"
+        placeholder="Find Pokemon"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <ul className="flex gap-5 flex-wrap justify-center items-center">
+        {
+          searchData.map((currPokemon) => {
+            return <PokemonCard key={currPokemon.id} pokemonData={currPokemon} />
+          })
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default App
--- pokemon_react/pokemon_card_website/src/Components/PokemonCard.jsx
+++ pokemon_react/pokemon_card_website/src/Components/PokemonCard.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 
-const PokemonCard = () => {
+const PokemonCard = ({pokemonData}) => {
   return (
     <div className="bg-white hover:scale-105 h-[360px] w-72 rounded-lg border flex gap-2 flex-col justify-start items-center overflow-hidden">
-      <img src="./images.png" className="h-40 w-full" />
-      <p className="font-bold text-2xl">Pikachu</p>
+      <img src={pokemonData.sprites.other.dream_world.front_default} className="h-40 w-full" />
+      <p className="font-bold text-2xl">{pokemonData.name}</p>
       <p className="bg-green-600 text-white px-5 py-[4px] rounded-xl font-semibold">
-        Free
+        {pokemonData.types.map(currType => currType.type.name).join(", ")}
       </p>
       <div className="flex gap-1 w-full items-center justify-between px-4 mt-3">
         <p className="font-semibold text-sm">
           Height:{" "}
-          <span className="text-gray-700 text-[12px] font-semibold">5</span>
+          <span className="text-gray-700 text-[12px] font-semibold">{pokemonData.height}</span>
         </p>
         <p className="font-semibold text-sm">
           Weight:{" "}
-          <span className="text-gray-700 text-[12px] font-semibold">5000</span>
+          <span className="text-gray-700 text-[12px] font-semibold">{pokemonData.weight}</span>
         </p>
         <p className="font-semibold text-sm">
           Speed:{" "}
-          <span className="text-gray-700 text-[12px] font-semibold">1000</span>
+          <span className="text-gray-700 text-[12px] font-semibold">{pokemonData.stats[5].base_stat}</span>
         </p>
       </div>
 
       <div className="flex w-full items-center justify-between px-4 mt-4 text-sm">
         <div className="flex flex-col items-center justify-center">
-          <span className="text-[12px] font-semibold text-gray-700">65</span>
+          <span className="text-[12px] font-semibold text-gray-700">{pokemonData.base_experience}</span>
           <p className="font-semibold">Experience</p>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <span className="text-[12px] font-semibold text-gray-700">65</span>
+          <span className="text-[12px] font-semibold text-gray-700">{pokemonData.stats[1].base_stat}</span>
           <p className="font-semibold">Attack</p>
         </div>
         <div className="flex flex-col items-center justify-center">
           <span className="text-[12px] font-semibold text-gray-700">
-            Lightning
+            {
+              pokemonData.abilities[0].ability.name
+            }
           </span>
           <p className="font-semibold">Abilities</p>
         </div>
